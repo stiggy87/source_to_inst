a338bd08beae8d4b7ebbfecf76d8276f63461b0b
Steve G <s.r.grace@gmail.com>
2012-12-14 15:59:44 -0700

    Added test_6.v where 2 modules are defined

---
+++ test_files/test_6.v
@@ -0,0 +1,21 @@
+// Verilog Test File 6
+
+// Module #1
+
+module 1st_module(port_1, port_2, port_3, port_4);
+	input port_1;
+	input port_2;
+	inout port_3;
+	output [1:0] port_4;
+	
+endmodule
+
+// Module #2
+
+module 2nd_module(port_1, port_2, port_3, port_4);
+	input port_1;
+	input port_2;
+	inout port_3;
+	output [1:0] port_4;
+	
+endmodule
